28cc3d811e1a210b2af7fbd04e6afe93995910bd
Hane Hatem <hanehatemelbasuony20@gmail.com>
2025-08-31 21:19:31 +0300

    local change in .env

--- src/DB/connectionDB.js
+++ src/DB/connectionDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const checkConnectionDB = async () => {
   await mongoose
-    .connect(process.env.DB_URL)
+    .connect(process.env.DB_URL_ONLINE)
     .then(() => {
       console.log('Suceccs DB Connection ^_^');
     })
